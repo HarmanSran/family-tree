a0c2cda883958262c9d96fb71559325026c339b7
Harman Sran <iharmandeep@gmail.com>
2021-05-01 17:29:11 -0400

    Add Fabs

--- src/components/FamilyTree/FamilyTree.js
+++ src/components/FamilyTree/FamilyTree.js
@@ -1,18 +1,66 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import VisNetworkReactComponent from 'vis-network-react';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core';
+import CenterFocusStrongIcon from '@material-ui/icons/CenterFocusStrong';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import augmentNetwork from './augmentNetwork/augmentNetwork';
 import options from './options/options';
 
-import './FamilyTree.css';
+const useStyles = makeStyles({
+  canvasWrapper: {
+    height: '100%',
+  },
+  fab: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    left: 'auto',
+    position: 'fixed',
+  },
+  recenterFab: {
+    bottom: 20,
+  },
+  githubFab: {
+    bottom: 20 + 56 + 20,
+  },
+});
 
-const FamilyTree = ({ network }) => (
-  <div className="canvas-wrapper">
-    <VisNetworkReactComponent
-      data={augmentNetwork(network)}
-      options={options}
-    />
-  </div>
-);
+const FamilyTree = ({ network }) => {
+  const classes = useStyles();
+  const [visNetwork, setVisNetwork] = useState(null);
+  const handleRecenterClick = () => {
+    if (visNetwork) {
+      visNetwork.fit();
+    }
+  };
+  return (
+    <div className={classes.canvasWrapper}>
+      <VisNetworkReactComponent
+        data={augmentNetwork(network)}
+        options={options}
+        getNetwork={(n) => setVisNetwork(n)}
+      />
+      <Fab
+        className={`${classes.fab} ${classes.recenterFab}`}
+        color="primary"
+        aria-label="recenter"
+        onClick={handleRecenterClick}
+      >
+        <CenterFocusStrongIcon />
+      </Fab>
+      <Fab
+        className={`${classes.fab} ${classes.githubFab}`}
+        color="default"
+        aria-label="recenter"
+        onClick={() => console.log('Github clicked')}
+      >
+        <GitHubIcon />
+      </Fab>
+    </div>
+  );
+};
 
 FamilyTree.propTypes = {
   network: PropTypes.exact({
